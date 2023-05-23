a046f02212da91d0e55d5ce375acbc14b6f55eee
amirakupov <amirakupov@gmail.com>
2023-05-23 19:13:53 +0200

    new repo

---
+++ OSOM_WEB_squiggle/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for OSOM_WEB_squiggle project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OSOM_WEB_squiggle.settings')
+
+application = get_asgi_application()
