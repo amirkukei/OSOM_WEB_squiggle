ccd5b7ab5fca4286d18dd1629ece2baf664bdcb2
mimimixer <113891038+mimimixer@users.noreply.github.com>
2023-06-16 10:55:20 +0200

    integrated css in all new pages + icon

--- squiggle/urls.py
+++ squiggle/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('design/list/', views.design_list, name='design_list'),
     path('order/create/', views.create_order, name='create_order'),
     path('order/list/', views.order_list, name='order_list'),
-    path('login/', views.user_login, name='login'),
-    #path('login/', auth_views.LoginView.as_view(), name='login'),
+#    path('login/', views.user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
